ac10778d3b404c5becfe58e5fed48a2f6ea663e0
masonhopkins <mason.hopkins@myfitnesspal.com>
2021-11-03 11:21:14 -0700

    Fixes UIColor label errors.

--- Sources/SwiftUICharts/Base/Label/ChartLabel.swift
+++ Sources/SwiftUICharts/Base/Label/ChartLabel.swift
@@ -59,13 +59,13 @@ public struct ChartLabel: View {
     private var labelColor: Color {
         switch labelType {
         case .title:
-            return Color(UIColor.label)
+            return Color(.labelColor)
         case .legend:
-            return Color(UIColor.secondaryLabel)
+            return Color(.secondaryLabelColor)
         case .subTitle:
-            return Color(UIColor.label)
+            return Color(.labelColor)
         case .largeTitle:
-            return Color(UIColor.label)
+            return Color(.labelColor)
         case .custom(_, _, let color):
             return color
         }
